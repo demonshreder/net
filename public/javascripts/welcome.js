c4d0a604638bb31378d5a45921e1d72e5bfea034
Kamalavelan <sskamalavelan@gmail.com>
2016-07-26 14:43:05 +0530

    Change homepage to console look
    Signed-off-by: Kamalavelan <sskamalavelan@gmail.com>

--- public/javascripts/welcome.js
+++ public/javascripts/welcome.js
@@ -1,19 +1,30 @@
-var exampleData = {
-    name: 'Vue.js'
-}
 
-var exampleVM = new Vue({
-    el: '#play',
-    data: exampleData
-})
+var exampleVM2 = new Vue({
+    el: '#term',
+    data: {
+        screen:  [{text:""}],
+        message: ""
+    },
+    methods: {
+        command: function () {
+            var text = this.message.trim()
+            if (text) {
+                this.screen.push({ text: text })
+                this.message = ''
+            var objDiv = document.getElementById("screen");
+            objDiv.scrollTop = objDiv.scrollHeight;
+            }
+        }
+    }
+});
 
 
 var mailStore = [];
 //Mail functionality
 function mail() {
-    ajax.send('/mail',function (data) {
-            mailStore = data;
-        },
+    ajax.send('/mail', function (data) {
+        mailStore = data;
+    },
         'GET');
 }
 
